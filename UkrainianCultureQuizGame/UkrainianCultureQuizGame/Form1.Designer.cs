b6f7ce9ae4ed751fa33768c1684071209febf9a0
Andr3ich <106585364+Andr3ich@users.noreply.github.com>
2023-04-28 15:52:51 +0300

    course-work

---
+++ UkrainianCultureQuizGame/UkrainianCultureQuizGame/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace UkrainianCultureQuizGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::UkrainianCultureQuizGame.Properties.Resources.question1;
+            this.pictureBox1.Location = new System.Drawing.Point(32, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(732, 432);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblQuestion.Location = new System.Drawing.Point(32, 415);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(732, 98);
+            this.lblQuestion.TabIndex = 1;
+            this.lblQuestion.Text = "Question";
+            this.lblQuestion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblQuestion.Click += new System.EventHandler(this.lblQuestion_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 560);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(322, 84);
+            this.button1.TabIndex = 2;
+            this.button1.Tag = "1";
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(442, 560);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(322, 84);
+            this.button2.TabIndex = 3;
+            this.button2.Tag = "2";
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(32, 675);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(322, 84);
+            this.button3.TabIndex = 4;
+            this.button3.Tag = "3";
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(442, 675);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(322, 84);
+            this.button4.TabIndex = 5;
+            this.button4.Tag = "4";
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 771);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Вікторина з Iсторії української культури";
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ UkrainianCultureQuizGame/UkrainianCultureQuizGame/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UkrainianCultureQuizGame
+{
+    public partial class Form1 : Form
+    {
+
+        int correctAnswer;
+        int questionNumber = 1;
+        int score;
+        int percentage;
+        int totalQuestions;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            askQuestion(questionNumber);
+
+            totalQuestions = 10;
+        }
+
+        private void lblQuestion_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkAnswerEvent(object sender, EventArgs e)
+        {
+            var senderObject = (Button)sender;
+
+            int buttonTag = Convert.ToInt32(senderObject.Tag);
+
+            if(buttonTag == correctAnswer)
+            {
+                score++;
+            }
+
+            if(questionNumber == totalQuestions)
+            {
+
+                percentage = (int)Math.Round((double)(score * 100) / totalQuestions);
+
+                MessageBox.Show(
+                    "Вiкторину завершено!" + Environment.NewLine +
+                    "Ви вiдповiли на " + score + " питань правильно." + Environment.NewLine +
+                    "Вiдсоток правильних вiдповiдей - " +percentage + "%" + Environment.NewLine +
+                    "Натиснiть OK щоб грати знову"
+                    );
+
+                score = 0;
+                questionNumber = 0;
+                askQuestion(questionNumber);
+            }
+
+            questionNumber++;
+            askQuestion(questionNumber);
+
+        }
+
+        private void askQuestion(int qnum)
+        {
+
+            switch (qnum)
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources.question1;
+
+                    lblQuestion.Text = "Вiдтворена гетьманська резиденцiя ХVII ст. знаходиться у містi:";
+
+                    button1.Text = "Чигирин";
+                    button2.Text = "Глухiв";
+                    button3.Text = "Батурин";
+                    button4.Text = "Канiв";
+
+                    correctAnswer = 1;
+
+                    break;
+
+                case 2:
+                    pictureBox1.Image = Properties.Resources.question2;
+
+                    lblQuestion.Text = "Де встановлено цей пам'ятник козакам та селянам повстанцям?";
+
+                    button1.Text = "в урочищi Холодний яр";
+                    button2.Text = "на територiї Нацiонального iсторико-меморiального заповiдника «Поле Берестецької битви»";
+                    button3.Text = "на островi Хортиця";
+                    button4.Text = "в мiстi Чигирин";
+
+                    correctAnswer = 2;
+
+                    break;
+
+                case 3:
+                    pictureBox1.Image = Properties.Resources.question3;
+
+                    lblQuestion.Text = "За цiєю свiтлиною можна впiзнати мiсто-музей";
+
+                    button1.Text = "Київ";
+                    button2.Text = "Чернiгiв";
+                    button3.Text = "Одеса";
+                    button4.Text = "Львiв";
+
+                    correctAnswer = 2;
+
+                    break;
+
+                case 4:
+                    pictureBox1.Image = Properties.Resources.question4;
+
+                    lblQuestion.Text = "Ймовiрне мiсце загибелi Святослава Хороброго знаходиться на територiї:";
+
+                    button1.Text = "Дунайського бiосферного заповiдника";
+                    button2.Text = "Карпатського бiосферного заповiдника";
+                    button3.Text = "Нацiональний заповiдник Хортиця";
+                    button4.Text = "Канiвського природного заповiдника";
+
+                    correctAnswer = 3;
+
+                    break;
+
+                case 5:
+                    pictureBox1.Image = Properties.Resources.question5;
+
+                    lblQuestion.Text = "На фото зображено:";
+
+                    button1.Text = "Дендропарк «Олександрiя»";
+                    button2.Text = "Нацiональний дендрологiчний парк «Софiївка»";
+                    button3.Text = "Бiосферний заповiдник «Асканiя-Нова»";
+                    button4.Text = "Державний дендрологiчний парк «Тростянець» НАН України";
+
+                    correctAnswer = 2;
+
+                    break;
+
+                case 6:
+                    pictureBox1.Image = Properties.Resources.question6;
+
+                    lblQuestion.Text = "Назва об'єкта культурної спадщини:";
+
+                    button1.Text = "Обелiск венедiв";
+                    button2.Text = "Збручанський iдол";
+                    button3.Text = "Половецька баба";
+                    button4.Text = "Скiфський перун";
+
+                    correctAnswer = 2;
+
+                    break;
+
+                case 7:
+                    pictureBox1.Image = Properties.Resources.question7;
+
+                    lblQuestion.Text = "Один iз мостiв через Днiпро у Києвi. Перший у свiтi суцiльнозварний мiст завдовжки 1543 метри?";
+
+                    button1.Text = "Пiвденний мiст";
+                    button2.Text = "Мiст iменi Патона";
+                    button3.Text = "Мерефо-Херсонський мiст";
+                    button4.Text = "Дев’ятиарковий мiст";
+
+                    correctAnswer = 2;
+
+                    break;
+
+                case 8:
+                    pictureBox1.Image = Properties.Resources.question8;
+
+                    lblQuestion.Text = "Один iз найдавнiших в Українi замкiв, iмовiрно, побудований одним iз синiв галицько-волинського князя Юрiя Львовича на плато пагорба, який височить серед заплави р. Лiберцiя. Його назва:";
+
+                    button1.Text = "Олеський";
+                    button2.Text = "Корецький";
+                    button3.Text = "Меджибiзький";
+                    button4.Text = "Високий";
+
+                    correctAnswer = 1;
+
+                    break;
+
+                case 9:
+                    pictureBox1.Image = Properties.Resources.question9;
+
+                    lblQuestion.Text = "Цей пам'ятник Т.Г.Шевченку вважається найкрасивiшим в Європi памятником Кобзарю. Вiн знаходиться у мiстi-музеї:";
+
+                    button1.Text = "Київ";
+                    button2.Text = "Полтава";
+                    button3.Text = "Харкiв";
+                    button4.Text = "Львiв";
+
+                    correctAnswer = 3;
+
+                    break;
+
+                case 10:
+                    pictureBox1.Image = Properties.Resources.question10;
+
+                    lblQuestion.Text = "Центральна площа мiста має форму кiльця, посеред якого розташований Корпусний сад iз монументом Слави. Це мiсто-музей:";
+
+                    button1.Text = "Днiпро";
+                    button2.Text = "Полтава";
+                    button3.Text = "Харкiв";
+                    button4.Text = "Одеса";
+
+                    correctAnswer = 2;
+
+                    break;
+            }
+        }
+
+    }
+}
